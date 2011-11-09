6c23dd23ec6e715cdf4675a083964ea3a0eb58d8
Mumo Carlos <mumo.crls@gmail.com>
2011-11-09 13:15:04 -0600

    rails 3.1 root config

--- lib/facebook_registration.rb
+++ lib/facebook_registration.rb
@@ -11,8 +11,8 @@ module FacebookRegistration
     def load_configuration(facebook_yaml_file)
       return false unless File.exist?(facebook_yaml_file)
       @raw_facebook_configuration = YAML.load(ERB.new(File.read(facebook_yaml_file)).result)
-      if defined? RAILS_ENV
-        @raw_facebook_configuration = @raw_facebook_configuration[RAILS_ENV]
+      if defined? Rails.env
+        @raw_facebook_configuration = @raw_facebook_configuration[Rails.env]
       end
       @raw_facebook_configuration
     end
@@ -23,7 +23,7 @@ module FacebookRegistration
   end
 end
 
-facebook_config = "#{RAILS_ROOT}/config/facebooker.yml"
+facebook_config = Rails.root.join('/config/facebooker.yml')
 FACEBOOK_CONFIG = FacebookRegistration.load_configuration(facebook_config)
 
 if defined? Rails
